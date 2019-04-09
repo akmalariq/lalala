99933ed2988f6a36dbb1050289c4de4792c25eb2
hellomermaido <49434151+hellomermaido@users.noreply.github.com>
2019-04-09 16:55:32 +0700

    lalala

---
+++ tubestry6/application/controllers/Home.php
@@ -0,0 +1,10 @@
+<?php
+
+class Home extends CI_Controller{
+	public function index(){
+		$this->load->view('awal');
+//		$this->load->view('home/index');
+//		$this->load->view('templates/footer');
+	}
+
+}
---
+++ tubestry6/application/views/awal.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+  <head>
+    <link rel="stylesheet" type="text/css" href="css.css" />
+
+    <title>Curious Cat!</title>
+  </head>
+  <body>
+    <div class = awal>
+            <img src="cat.jpg" alt="catto" align = "top"/>
+        <div class = kanan>
+            <h1> The Place To Meet Those Around You!</h1>
+            <p> Curious Cat is the perfect place to meet new people in new and exciting ways!</p>
+        </div>
+    </div>
+
+  </body>
+</html>
